6ddcdd80554def5b370785e5dc44af9125bf11de
arun861 <73119557+arun861@users.noreply.github.com>
2022-10-07 11:47:28 +0530

    update db config to work with intern sql servers

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php 
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
+$mysql_hostname = "10.201.7.176";
+$mysql_user = "yodasql";
+$mysql_password = "12345678Yoda@";
 $mysql_database = "profile";
 $con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
 ?>
